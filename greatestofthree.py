8c2d4dedf69bc743f55c5804000135bdf417dc81
Piyush-Thakur7 <th.piyushsingh2007@gmail.com>
2025-06-20 14:48:05 +0530

    Create greatestofthree.py

---
+++ greatestofthree.py
@@ -0,0 +1,9 @@
+a=float(input("enter your number for A : "))
+b=float(input("enter your number for B : "))
+c=float(input("enter your number for C : "))
+if(a>=b and a>=c):
+    print("A is greatest", a)
+elif(b >=c):
+    print("B is greatest", b)
+else:
+        print("C is greatest", c)
